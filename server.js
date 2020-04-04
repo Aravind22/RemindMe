46e8863658d490811fe3090b97c7a63c480698bf
Aravind <ajey33@gmail.com>
2020-04-04 17:16:27 +0530

    remind-m-e init

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/remindMe'));
+app.use(express.static(__dirname + '/dist/remind-m-e'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/remindMe/index.html'));
+res.sendFile(path.join(__dirname+'/dist/remind-m-e/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
